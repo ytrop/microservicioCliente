8c2971d53c69d3f794396fe7a7986d6838eeb198
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-15 11:00:23 +0100

    Add files via upload
    First commit

---
+++ prueba-clientemicro/src/test/java/com/example/prueba/clientemicro/PruebaClientemicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba.clientemicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaClientemicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
